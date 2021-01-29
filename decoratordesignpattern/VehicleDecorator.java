6c93769f3b6afa3aa4f6caee48d6f1b9ee76f289
Jackson Carroll <Jack Carroll>
2021-01-29 11:18:54 -0500

    added override and comments

--- decoratordesignpattern/VehicleDecorator.java
+++ decoratordesignpattern/VehicleDecorator.java
@@ -5,5 +5,10 @@ package decoratordesignpattern;
  * @author Jackson Carroll
  */
 public abstract class VehicleDecorator extends Vehicle {
+
+    /**
+     * Abstract method to create a string of a decorated vehicle and overrides
+     */
+    @Override
     public abstract String toString();
 }
